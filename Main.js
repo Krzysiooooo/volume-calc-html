1b539194b9b9cb89235acbe63788efb309d53e68
jbeynar <jakub.bejnarowicz@gmail.com>
2019-11-28 19:38:29 +0000

    figure-inputs-described-on-calc-map-wip

--- Main.js
+++ Main.js
@@ -1,18 +1,28 @@
-
 const calcFieldMap = {
-    walec: (input) => {
-        return Math.PI * Math.pow(input.r, 2) * input.h
+    walec: {
+        inputs: ['r', 'h'],
+        formula: (input) => {
+            return Math.PI * Math.pow(input.r, 2) * input.h
+        }
     },
-    kula: (input) => {
-        return 4 / 3 * Math.PI * Math.pow(input.r, 3);
+    kula: {
+        inputs: ['r'],
+        formula: (input) => {
+            return 4 / 3 * Math.PI * Math.pow(input.r, 3);
+        }
     },
-    stozek: (input) => {
-        return 1 / 3 * Math.PI * Math.pow(input.r, 2) * input.h
+    stozek: {
+        inputs: ['r', 'h'],
+        formula: (input) => {
+            return 1 / 3 * Math.PI * Math.pow(input.r, 2) * input.h
+        }
     },
-    prostopadloscian: (input) => {
-        return input.r * input.h * input.a
+    prostopadloscian: {
+        inputs: ['a', 'b', 'c'],
+        formula: (input) => {
+            return input.a * input.b * input.c
+        }
     }
-
 };
 
 var activeFigure = {
@@ -25,17 +35,22 @@ function clickListener(event) {
     activeFigure.name = event.target.getAttribute('data-figure');
     document.querySelector("#selected-figure").innerHTML = activeFigure.name;
     document.querySelector(".calculator").style.display = "block";
+    document.querySelector("div." + activeFigure.name).style.display = "block";
 }
 
 document.querySelector("#calc").addEventListener("click", () => {
+    const formulaFn = calcFieldMap[activeFigure.name].formula;
+    const inputs = calcFieldMap[activeFigure.name].inputs;
+
+    // TODO instead of hardcoded inputs (as below), read inputs from figure object
     const input = {
         r: parseInt(document.querySelector("#input-r").value),
         h: parseInt(document.querySelector("#input-h").value),
         a: parseInt(document.querySelector("#input-a").value)
     };
-    const fieldFn = calcFieldMap[activeFigure.name];
+
     activeFigure.input = input;
-    activeFigure.field = fieldFn(input);
+    activeFigure.field = formulaFn(input);
     document.querySelector("#selected-figure-input").innerHTML = `Wprowadzone wymiary:<ul> <li>r = ${activeFigure.input.r}</li> <li>h = ${activeFigure.input.h}</li> <li>a = ${activeFigure.input.a}</li></ul>`;
     document.querySelector("#selected-figure-field").innerText = activeFigure.field;
     document.querySelector("#input-r").value = '';
